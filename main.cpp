63e0e9b16210d24b3633999edf994199974e7e4a
Pavlo Ilin <ilin.pa@gmail.com>
2021-02-24 00:05:18 +0100

    Add barrier to multithreaded test

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 
 #define  _CRT_SECURE_NO_WARNINGS
 
-
+#include <cassert>
 #include <cstdint>
 #include <cstdio>
 
 #include <array>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 
 #define NOMINMAX
 #define WIN32_LEAN_AND_MEAN
@@ -15,9 +17,36 @@
 
 
 
-
-void Test(void* p)
+struct SBarrier
 {
+	std::mutex mutex;
+	std::condition_variable cv;
+	std::condition_variable cvProd;
+	int counter = 0;
+
+	void Enter()
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		counter++;
+		cvProd.notify_one();
+		cv.wait(lock);
+	}
+
+	void ProdWait(int target)
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		cvProd.wait(lock, [&]() { return counter >= target; });
+		
+		assert(counter == target);
+		counter = 0;
+		cv.notify_all();
+	}
+};
+
+void Test(void* p, SBarrier* pb)
+{
+	SetThreadDescription(GetCurrentThread(), L"Test");
+
 	auto* a = reinterpret_cast<uint32_t*>(p);
 	a[0] = 42;
 
@@ -26,66 +55,76 @@ void Test(void* p)
 		::printf("&a[%d] = %p\n", i, a + i);
 	}
 
+	if (pb)
+	{
+		pb->Enter();
+	}
 
-	::printf("------------ stage 0 - tid %d\n", GetCurrentThreadId());
-	a[0] = 0;
-	a[1] = 1;
-	a[2] = 2;
-	a[3] = 3;
-	a[4] = 4;
+	for (int i = 0; i < 1000; ++i)
+	{
+		//::printf("------------ stage 0 - tid %d\n", GetCurrentThreadId());
+		a[0] = 0;
+		a[1] = 1;
+		a[2] = 2;
+		a[3] = 3;
+		a[4] = 4;
 
-	memprotect::ProtectAddress(&a[3]);
-
-
-	::printf("------------ stage 1 - tid %d\n", GetCurrentThreadId());
-	a[0] = 10;
-	a[1] = 11;
-	a[2] = 12;
-	a[3] = 13;
-	a[4] = 14;
-
-	memprotect::ProtectAddress(&a[1]);
-
-	::printf("------------ stage 2 - tid %d\n", GetCurrentThreadId());
-	a[0] = 20;
-	a[1] = 21;
-	a[2] = 22;
-	a[3] = 23;
-	a[4] = 24;
+		memprotect::ProtectAddress(&a[3]);
 
 
-	memprotect::UnprotectAddress(&a[3]);
+		//::printf("------------ stage 1 - tid %d\n", GetCurrentThreadId());
+		a[0] = 10;
+		a[1] = 11;
+		a[2] = 12;
+		a[3] = 13;
+		a[4] = 14;
 
-	::printf("------------ stage 3 - tid %d\n", GetCurrentThreadId());
-	a[0] = 30;
-	a[1] = 31;
-	a[2] = 32;
-	a[3] = 33;
-	a[4] = 34;
+		memprotect::ProtectAddress(&a[1]);
 
-	memprotect::UnprotectAddress(&a[1]);
+		//::printf("------------ stage 2 - tid %d\n", GetCurrentThreadId());
+		a[0] = 20;
+		a[1] = 21;
+		a[2] = 22;
+		a[3] = 23;
+		a[4] = 24;
 
-	::printf("------------ stage 4 - tid %d\n", GetCurrentThreadId());
-	a[0] = 40;
-	a[1] = 41;
-	a[2] = 42;
-	a[3] = 43;
-	a[4] = 44;
+
+		memprotect::UnprotectAddress(&a[3]);
+
+		//::printf("------------ stage 3 - tid %d\n", GetCurrentThreadId());
+		a[0] = 30;
+		a[1] = 31;
+		a[2] = 32;
+		a[3] = 33;
+		a[4] = 34;
+
+		memprotect::UnprotectAddress(&a[1]);
+
+		//::printf("------------ stage 4 - tid %d\n", GetCurrentThreadId());
+		a[0] = 40;
+		a[1] = 41;
+		a[2] = 42;
+		a[3] = 43;
+		a[4] = 44;
+	}
 }
 
 
-void TestWithOwnMem()
+void TestWithOwnMem(SBarrier* pb)
 {
 	void* p = VirtualAlloc(nullptr, 16, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-	Test(p);
+	Test(p, pb);
 	VirtualFree(p, 0, MEM_RELEASE);
 }
 
 void TestMultipleThreadsOwnMem()
 {
+	SBarrier bar;
 	std::array<std::thread, 16> threads;
 	for (std::thread& t : threads)
-		t = std::thread(&TestWithOwnMem);
+		t = std::thread(&TestWithOwnMem, &bar);
+
+	bar.ProdWait((int)threads.size());
 
 	for (std::thread& t : threads)
 		t.join();
@@ -96,12 +135,17 @@ void TestMultipleThreadsSharedPage()
 	void* p = VirtualAlloc(nullptr, 16, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 	std::array<std::thread, 16> threads;
 
+	SBarrier bar;
+
 	char* a = (char*)p;
 	for (std::thread& t : threads)
 	{
-		t = std::thread(&Test, a);
+		t = std::thread(&Test, a, &bar);
 		a += 64;
 	}
+
+	bar.ProdWait((int)threads.size());
+
 	for (std::thread& t : threads)
 		t.join();
 	VirtualFree(p, 0, MEM_RELEASE);
@@ -110,9 +154,11 @@ void TestMultipleThreadsSharedPage()
 
 int main()
 {
+	SetThreadDescription(GetCurrentThread(), L"Main");
+
 	memprotect::Init();
 
-	//TestWithOwnMem();
+	//TestWithOwnMem(nullptr);
 	//TestMultipleThreadsOwnMem();
 	TestMultipleThreadsSharedPage();
 
